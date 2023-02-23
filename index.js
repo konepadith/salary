b47368fd4e865afe37b4ac73084aeb0fcdac30d3
konepadith <konepadithspydee@gmail.com>
2023-02-23 13:57:39 +0700

    pelay comiit

---
+++ index.js
@@ -0,0 +1,51 @@
+const qrcode = require('qrcode-terminal');
+const { Client,LocalAuth,MessageMedia } = require('whatsapp-web.js');
+
+const client = new Client({
+  authStrategy: new LocalAuth()})
+  
+const data = require('./person.json')
+const cron = require("node-cron");
+const moment = require("moment")
+
+
+
+function subtractMonths(date, months) {
+    date.setMonth(date.getMonth() - months);
+    return date;
+  }
+  let dateFormat1 = moment().format('DD-MM-YYYY');
+console.log(dateFormat1); // 23-08-2022
+  
+//   ✅ Subtract 1 month from the current Date
+  const result1 = subtractMonths(new Date(), 1);
+  console.log(moment(result1).format('MMM')); //👉️ 2022-12-13T13:00:19.456Z
+
+
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+    for (let i = 0; i < Object.keys(data).length; i++) {
+        const element = data[i];
+        // console.log(element.FullNames+".pdf")
+        // console.log(element.CodesTel.substring(1)+"@c.us")
+        var phoneNumber=element.CodesTel.substring(1)+"@c.us"
+        
+        // client.sendMessage(phoneNumber,TextMessage)
+        // client.sendMessage("85620"+element.tel+"@c.us",element.name+"\n"+element.salary.toLocaleString("en-US")+"\nສະບາຍດີ");
+        client.sendMessage(phoneNumber,"ຈາກພະແນກບຸກຄະລາກອນ ບໍລິສັດ ໂອເຊຍໂນ\nສະບາຍດີ "
+         + element.FullNames + 
+         "ນີ້ແມ່ນເອກະສານໃບແຈ້ງຍອດລວມເງິນເດືອນປະຈຳເດືອນ "
+         + moment(result1).format('MM') 
+         + "\nຖ້າມີຂໍ້ມູນໃດບໍ່ຖືກຕ້ອງ ກະລຸນາແຈ້ງຂໍ້ມູນປະໄວ້ ທາງຝ່າຍເຮົາຈະໃຫ້ຄຳຕອບໃນໄວໆນີ້\nຂໍຂອບໃຈ");
+        const attachmentPdf = MessageMedia.fromFilePath("salary/"+element.FullNames+".pdf");
+        client.sendMessage(phoneNumber, attachmentPdf); 
+        console.log(i+1+"."+phoneNumber)
+      }
+});
+
+client.initialize();
